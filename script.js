52f4f9de51491bad8a4f4d5567a8da3691984b8c
Adxambek <gulibahor1957@gmail.com>
2023-09-01 14:14:02 -0700

    test

---
+++ script.js
@@ -0,0 +1,11 @@
+console.error("Hello World!"); //bu yerda console.error bizga hatolik borligini chiqarish uchun kerak bu bizga qizil rangda chiqadi
+console.warn("Warning!"); //bu yerda console.warn bizga ogohlantirish belgisi bizga sariq rangda chiqadi
+console.log("console"); // console.log bizga ekranga nimanidir chiqarish kerak boladi debug qilishimiz uchun kerak
+// debug bizga qandaydir hatolik bolsa ekranga chiqarish uchun ishlatiladi daturda qandaydir
+//hatolik bolsa ekranga chiqarish uchun debug ishlatiladi
+
+// js da ozgaruvchilar var, let va const kalit sozlaridan foydalaniladi
+// var kalit sozi global miqyosda elon qiladi
+// let kalit sozi esa local miqyosda elon qiladi
+// const bu konstanta yani ozgarmas nimanidir elon qilib unga const kalit sozini ishlatsak u ozgarmas boladi
+// masalan soat 60 daqiqa u ozgarmaydi ozgarmaydigan qiymatlarga const ishlatiladi
