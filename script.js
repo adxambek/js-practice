bc06fcf2f5e4c4aca8fd712689fcfbe31c38dc39
Adxambek <gulibahor1957@gmail.com>
2023-09-04 00:13:05 -0700

    up-to-date

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-console.error("Hello World!"); //bu yerda console.error bizga hatolik borligini chiqarish uchun kerak bu bizga qizil rangda chiqadi
-console.warn("Warning!"); //bu yerda console.warn bizga ogohlantirish belgisi bizga sariq rangda chiqadi
-console.log("console"); // console.log bizga ekranga nimanidir chiqarish kerak boladi debug qilishimiz uchun kerak
+// console.error("Hello World!"); //bu yerda console.error bizga hatolik borligini chiqarish uchun kerak bu bizga qizil rangda chiqadi
+// console.warn("Warning!"); //bu yerda console.warn bizga ogohlantirish belgisi bizga sariq rangda chiqadi
+// console.log("console"); // console.log bizga ekranga nimanidir chiqarish kerak boladi debug qilishimiz uchun kerak
 // debug bizga qandaydir hatolik bolsa ekranga chiqarish uchun ishlatiladi daturda qandaydir
 //hatolik bolsa ekranga chiqarish uchun debug ishlatiladi
 
@@ -9,3 +9,11 @@ console.log("console"); // console.log bizga ekranga nimanidir chiqarish kerak b
 // let kalit sozi esa local miqyosda elon qiladi
 // const bu konstanta yani ozgarmas nimanidir elon qilib unga const kalit sozini ishlatsak u ozgarmas boladi
 // masalan soat 60 daqiqa u ozgarmaydi ozgarmaydigan qiymatlarga const ishlatiladi
+
+// String, Number, Boolean, undefined, null, Symbol
+
+let str = "hello world"; //string korinishidagi matn
+console.log(str);
+
+const num = 10; //number korinishidagi son
+console.log(num);
